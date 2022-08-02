137ae3adc82a8c1c30a9fe2c1f19b98594139552
Hasret Özkan <hasretozkan1@gmail.com>
2022-08-02 23:55:16 +0300

    new example image and app

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 Cute and Hot React Native UI Library 🔥
 
+## Demo Image
+
 ![react native fade loading demo example](https://raw.githubusercontent.com/hasretozkan/react-native-bonfire/master/example.png)
 
 ## Installation
@@ -20,12 +22,19 @@ yarn add react-native-bonfire
 
 ## Documantation
 
-[View the full docs here](https://hasret-oezkan.gitbook.io/react-native-bonfire/)
+[View the full docs here](https://rnbonfire.gitbook.io/react-native-bonfire/)
 
 ## Contributing
 
 See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
 
-## License
+## Expo demo app
 
-MIT
+Checkout the official [React Native Elements App](https://expo.dev/@rne_org/react-native-elements) on Expo which uses all of the React Native Elements components.
+
+If you are looking to contribute to the React Native Elements App, [here](https://github.com/react-native-elements/react-native-elements/tree/next/example) to view the implementation & Use this command to start example app locally
+
+```bash
+cd example
+yarn start
+```
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,34 +1,50 @@
 import * as React from 'react';
 
-import { StyleSheet, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 import { Text, Divider } from 'react-native-bonfire';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text type="h2" style={[styles.text, styles.boldText]}>
-        Typograpy & Text Components
-      </Text>
-      <Text type="h1" style={styles.text}>
-        Header 1
-      </Text>
-      <Text type="h2" style={styles.text}>
-        Header 2
-      </Text>
-      <Text type="h3" style={styles.text}>
-        Header 3
-      </Text>
-      <Text type="h4" style={styles.text}>
-        Header 4
-      </Text>
-      <Text type="h5" style={styles.text}>
-        Header 5
-      </Text>
-      <Text type="p" style={styles.text}>
-        Paragrapf
-      </Text>
-      <Divider marginY={20} color="lightgray" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={styles.container}>
+          <Divider marginY={20} color="white" />
+          <Text type="h4" style={[styles.text, styles.boldText]}>
+            React Native Bonfire 🔥
+          </Text>
+          <Divider marginY={20} color="lightgray" />
+          <Text type="h5" style={[styles.text, styles.boldText]}>
+            Typograpy & Text Components 🔥
+          </Text>
+          <Text type="h1" style={styles.text}>
+            Header 1
+          </Text>
+          <Text type="h2" style={styles.text}>
+            Header 2
+          </Text>
+          <Text type="h3" style={styles.text}>
+            Header 3
+          </Text>
+          <Text type="h4" style={styles.text}>
+            Header 4
+          </Text>
+          <Text type="h5" style={styles.text}>
+            Header 5
+          </Text>
+          <Text type="p" style={styles.text}>
+            Paragrapf
+          </Text>
+          <Divider marginY={20} color="lightgray" />
+          <Text type="h5" style={[styles.text, styles.boldText]}>
+            Divider Component 🔥
+          </Text>
+          <Divider marginY={5} color="black" />
+          <Divider marginY={5} color="red" />
+          <Divider marginY={5} color="blue" />
+          <Divider marginY={5} color="yellow" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -37,6 +53,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
   },
   text: {
     marginTop: 10,
